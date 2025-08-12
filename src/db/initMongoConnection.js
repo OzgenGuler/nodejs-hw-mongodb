54d86f37a65fb6ced709351548b0fa725e35e409
OzgenGuler <oguler93@gmail.com>
2025-08-12 22:04:20 +0300

    rr

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -8,13 +8,14 @@ const initMongoConnection = async () => {
   const MONGODB_DB = env('MONGODB_DB');
   const mongoUri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
-  const options = {
-    serverSelectionTimeoutMS: 5000,
-    socketTimeoutMS: 45000,
-  };
+  // const options = {
+  //   serverSelectionTimeoutMS: 5000,
+  //   socketTimeoutMS: 45000,
+  // };
 
   try {
-    await mongoose.connect(mongoUri, options);
+    // await mongoose.connect(mongoUri, options);
+    await mongoose.connect(mongoUri);
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
